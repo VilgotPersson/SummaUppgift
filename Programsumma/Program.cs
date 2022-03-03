e04934bdd3f3d87f7cd6c93d1b9e7d4c18f13c5b
VilgotPersson <69978046+VilgotPersson@users.noreply.github.com>
2022-03-03 10:09:01 +0100

    Gjorde klart uppgiften

---
+++ Programsumma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace Programsumma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath=@"C:\Users\Admin\Desktop\Programsumma\Summa.txt";
+            
+            int i = 0;
+            int summa = 0;
+            string rad= "";
+
+            StreamReader sr = new StreamReader(filepath);
+            List<string> textRader = new List<string>();
+
+            while((rad=sr.ReadLine()) != null)
+            {
+                summa += int.Parse(rad);
+                textRader.Add(rad);
+                if(rad.Contains("5"))
+                i++;
+            } 
+            sr.Close();
+            Console.WriteLine("Summan är: " + summa);
+            Console.WriteLine("Antalet femmor är: " + i);
+        }
+    }
+}
